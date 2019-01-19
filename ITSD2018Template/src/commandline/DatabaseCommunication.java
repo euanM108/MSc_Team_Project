be9b6dea3eab713805ef617c54bba1b0c51803ea
mlaidlawscott <michaellaidlawscott96@gmail.com>
2019-01-19 15:21:10 +0000

    Added functionality to the databaseCommunication Class

--- ITSD2018Template/src/commandline/DatabaseCommunication.java
+++ ITSD2018Template/src/commandline/DatabaseCommunication.java
@@ -3,9 +3,7 @@ package commandline;
 import java.sql.*;
 
 //this class will handle inputting and retrieving relevant information from the postgreSQL database 
-//Option 1: make everything static and check if we are connected to database for each method
-//Option 2: use a constructor which connects to the database.
-//Option 1 is better because it limits the risk of errors.
+//need to connect to the university database server. 
 
 public class DatabaseCommunication {
 
@@ -22,7 +20,7 @@ public class DatabaseCommunication {
 		}
 
 		try {
-			connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/DTAAE1", "postgres", "password");
+			connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/ITGroupProject", "postgres", "password here");
 		} catch (SQLException e) {
 			System.err.println("Connection Failed!");
 			e.printStackTrace();
@@ -30,58 +28,153 @@ public class DatabaseCommunication {
 		}
 	}
 
-	public static void writeGameResults(int winnerID, int draws , int noRounds, int AI1Wins, int AI2Wins, int AI3Wins, int AI4Wins) {
+	public static void writeGameResults(int winnerID, int draws , int noRounds, int playerWins, int AI1Wins, int AI2Wins, int AI3Wins, int AI4Wins) {
 		// writes the results of the game to the database
+		//winner id is 1 for player and then 2-5 for each AI player in ascending order
 		if (connection != null) {
-			//sql statement to write game results goes here
+			try {
+				Statement SQLStatement = connection.createStatement();
+				String SQLQuery = String.format(
+						"INSERT INTO game_statistics (winnerid, nodraws, totalrounds, playerwins, aiplayeronewins, aiplayertwowins, aiplayerthreewins, aiplayerfourwins)\r\n"
+								+ "VALUES (%d , %d, %d, %d, %d, %d, %d, %d)",
+						winnerID, draws, noRounds, playerWins, AI1Wins, AI2Wins, AI3Wins, AI4Wins);
+				SQLStatement.executeUpdate(SQLQuery);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
 		} else {
 			connectToDatabase();
-			writeGameResults(winnerID, draws, noRounds, AI1Wins, AI2Wins, AI3Wins, AI4Wins);
+			writeGameResults(winnerID, draws, noRounds, playerWins, AI1Wins, AI2Wins, AI3Wins, AI4Wins);
 		}
 	}
 	
-	public static void getPreviousGameStatistics() {
+	public static String getPreviousStatistics() {
 		//use 5 different methods then combine to 1 string or return 5 strings
 		if (connection != null) {
-			//sql statement to write game results goes here
+			int noGames = getNoGames();
+			int AIWins = getNoAIWins();
+			int humanWins = getNoHumanWins();
+			double AVGDraws = getAVGDraws();
+			int largestNoRounds = getLargestNoRounds();
+			//basic return string for now, can be altered as required.
+			String statistics = String.format("%d\t%d\t%d\t%.2f\t%d", noGames, AIWins, humanWins, AVGDraws, largestNoRounds);
+			System.out.println(statistics);
+			return statistics;
 		} else {
 			connectToDatabase();
-			getPreviousGameStatistics();
+			getPreviousStatistics();
 		}
+		return null;
 	}
 	
-	private static String getNoGames() {
-		String noGames = null;
+	private static int getNoGames() {
+		int noGames = 0;
 		try {
-			Statement noGamesStatement = connection.createStatement();
-			String SQLQuery = "query placeholder";
-			ResultSet queryResult = noGamesStatement.executeQuery(SQLQuery); 
-			noGames = queryResult.getString("id");
+			Statement SQLStatement = connection.createStatement();
+			String SQLQuery = "SELECT MAX(id)\r\n" + 
+					"FROM game_statistics";
+			ResultSet queryResult = SQLStatement.executeQuery(SQLQuery);
+			queryResult.next();
+			noGames = queryResult.getInt(1);
+			return noGames;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return noGames;
 	}
-	
-	private static String getNoAIWins() {
-		return "placeholder";
+
+	private static int getNoAIWins() {
+		int noAIWins = 0;
+		try {
+			Statement SQLStatement = connection.createStatement();
+			String SQLQuery = "Select COUNT(winnerID)\r\n" + 
+					"FROM game_statistics\r\n" + 
+					"WHERE winnerID > 1";
+			ResultSet queryResult = SQLStatement.executeQuery(SQLQuery);
+			queryResult.next();
+			noAIWins = queryResult.getInt(1);
+			return noAIWins;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return noAIWins;
 	}
 	
-	private static String getNoHumanWins() {
-		return "placeholder";
+	private static int getNoHumanWins() {
+		int noHumanWins = 0;
+		try {
+			Statement SQLStatement = connection.createStatement();
+			String SQLQuery = "SELECT COUNT(winnerID)\r\n" + 
+					"FROM game_statistics\r\n" + 
+					"WHERE winnerID = 1";
+			ResultSet queryResult = SQLStatement.executeQuery(SQLQuery);
+			queryResult.next();
+			noHumanWins = queryResult.getInt(1);
+			return noHumanWins;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return noHumanWins;
 	}
 	
-	private static String getAVGDraws() {
-		return "placeholder";
+	private static double getAVGDraws() {
+		double AVGDraws = 0;
+		try {
+			Statement SQLStatement = connection.createStatement();
+			String SQLQuery = "SELECT AVG(nodraws)\r\n" + 
+					"FROM game_statistics";
+			ResultSet queryResult = SQLStatement.executeQuery(SQLQuery);
+			queryResult.next();
+			AVGDraws = queryResult.getDouble(1);
+			return AVGDraws;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return AVGDraws;
 	}
 	
-	private static String getLargestNoRounds() {
-		return "placeholder";
+	private static int getLargestNoRounds() {
+		int largestNoRounds = 0;
+		try {
+			Statement SQLStatement = connection.createStatement();
+			String SQLQuery = "SELECT MAX(totalrounds)\r\n" + 
+					"FROM game_statistics";
+			ResultSet queryResult = SQLStatement.executeQuery(SQLQuery);
+			queryResult.next();
+			largestNoRounds = queryResult.getInt(1);
+			return largestNoRounds;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return largestNoRounds;
+	}
+	
+	public static void clearHistory() {
+		//this method completely clears the database table
+		if (connection != null) {
+			try {
+				Statement SQLStatement = connection.createStatement();
+				String SQLQuery = "DELETE FROM game_statistics";
+				SQLStatement.executeUpdate(SQLQuery);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		} else {
+			connectToDatabase();
+			clearHistory();
+		}
 	}
 
 	public static void main(String[] args) {
 		//the main can be used for testing and will be removed when 
-		//into the full programme
+		//implemented into the full programme
+		getPreviousStatistics();
+		writeGameResults(3, 1, 1, 4, 6, 1, 4, 8);
+		getPreviousStatistics();
+		clearHistory();
+		getPreviousStatistics();
 	}
 
 }
