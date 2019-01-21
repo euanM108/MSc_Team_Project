ef31b982c66e2b4a11f48636f94f5a39ee3c0961
Peter McCreadie <pm@petermccreadie.com>
2019-01-21 15:53:39 +0000

    "working state"

--- ITSD2018Template/src/commandline/FileHandler.java
+++ ITSD2018Template/src/commandline/FileHandler.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class FileHandler {
 
     private final static String classDirectory = System.getProperty("user.dir") + "/";
-    private static String nameOfDeck = "AnimalList2.txt";
+    private static String nameOfDeck = "StarCitizenDeck.txt";
     private static String [] extractedCatArray;
     private ArrayList<Card> deck = new ArrayList<>();
     private ArrayList<String> catNames = new ArrayList<String>();
