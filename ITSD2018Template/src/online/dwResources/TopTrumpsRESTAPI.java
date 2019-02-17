f194ec53deab573ab1c900c010c75ac391c3be2a
Peter McCreadie <pm@petermccreadie.com>
2019-02-17 19:02:48 +0000

    Database write statements adjusted in RestAPI

--- ITSD2018Template/src/online/dwResources/TopTrumpsRESTAPI.java
+++ ITSD2018Template/src/online/dwResources/TopTrumpsRESTAPI.java
@@ -736,40 +736,35 @@ public class TopTrumpsRESTAPI {
 	@GET
 	@Path("/getTotalGames")
 	public String getTotalGames() throws IOException {
-		//int games = DatabaseCommunication.getNoGames();
-		int games = 6;
+		int games = DatabaseCommunication.getNoGames();
 		return ""+games;
 	}
 	
 	@GET
 	@Path("/getCompWins")
 	public String getCompWins() throws IOException {
-		//int AIwins = DatabaseCommunication.getNoAIWins();
-		int AIwins = 7;
+		int AIwins = DatabaseCommunication.getNoAIWins();
 		return ""+AIwins;
 	}
 	
 	@GET
 	@Path("/getHumanWins")
 	public String getHumanWins() throws IOException {
-		//int humanWins = DatabaseCommunication.getNoHumanWins();
-		int humanWins = 8;
+		int humanWins = DatabaseCommunication.getNoHumanWins();
 		return ""+humanWins;
 	}
 	
 	@GET
 	@Path("/getAveDraws")
 	public String getAveDraws() throws IOException {
-		//double drawAvg = DatabaseCommunication.getAVGDraws();
-		double drawAvg = 9;
+		double drawAvg = DatabaseCommunication.getAVGDraws();
 		return ""+drawAvg;
 	}
 	
 	@GET
 	@Path("/getBigRound")
 	public String getBigRound() throws IOException {
-		//int roundMax = DatabaseCommunication.getLargestNoRounds();
-		int roundMax = 10;
+		int roundMax = DatabaseCommunication.getLargestNoRounds();
 		return ""+roundMax;
 	}
 	
